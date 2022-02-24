aedb5a88da51600e3eae19ccbc857446c6ab38d0
ilya <antonpuckich@gmail.com>
2022-02-24 21:42:06 +0300

    Write tests for ReplyKeyboardsTest class

---
+++ src/test/java/com/telegram/bot/handlers/scripts/helperclasses/ReplyKeyboardsTest.java
@@ -0,0 +1,23 @@
+package com.telegram.bot.handlers.scripts.helperclasses;
+
+import org.junit.jupiter.api.Test;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class ReplyKeyboardsTest {
+    @Test
+    public void whenGettingButtonWithText() {
+        String text = "text";
+        ReplyKeyboardMarkup keyboard = (ReplyKeyboardMarkup) ReplyKeyboards.getButtonWithText(text);
+        assertThat(keyboard.getKeyboard().get(0).get(0).getText(), is(text));
+    }
+
+    @Test
+    public void whenRemovingKeyboard() {
+        ReplyKeyboardRemove keyboardRemove = (ReplyKeyboardRemove) ReplyKeyboards.removeKeyboard();
+        assertThat(keyboardRemove.getRemoveKeyboard(), is(true));
+    }
+}
